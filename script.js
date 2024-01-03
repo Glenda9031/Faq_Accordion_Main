29153d7f5db5eefd4c450c2060138660e0c0bb7e
Glenda Joyce Reed <glendareed0807@sbcglobal.net>
2024-01-03 18:30:18 -0500

    Update style.css and script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('title').addEventListener('click', function () {
+    if (this.classList.contains('title__click')) {
+        this.classList.remove('title__click');
+    } else {
+        this.classList.add('title__click');
+    };
+})
+
+document.getElementById('title2').addEventListener('click', function () {
+    if (this.classList.contains('title__click')) {
+        this.classList.remove('title__click');
+    } else {
+        this.classList.add('title__click');
+    };
+})
+
+document.getElementById('title3').addEventListener('click', function () {
+    if (this.classList.contains('title__click')) {
+        this.classList.remove('title__click');
+    } else {
+        this.classList.add('title__click');
+    };
+})
+
+document.getElementById('title4').addEventListener('click', function () {
+    if (this.classList.contains('title__click')) {
+        this.classList.remove('title__click');
+    } else {
+        this.classList.add('title__click');
+    };
+})
